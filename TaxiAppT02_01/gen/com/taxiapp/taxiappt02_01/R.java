24fb8e64d03bf46fc95cb8418ed99b0a9bf3f778
peggyle <peggyle.18@gmail.com>
2014-11-20 13:38:35 -0500

    login screen testing

--- TaxiAppT02_01/gen/com/taxiapp/taxiappt02_01/R.java
+++ TaxiAppT02_01/gen/com/taxiapp/taxiappt02_01/R.java
@@ -858,10 +858,11 @@ containing a value of this type.
         public static final int abc_split_action_bar_is_narrow=0x7f060002;
     }
     public static final class color {
-        public static final int abc_search_url_text_holo=0x7f070003;
+        public static final int abc_search_url_text_holo=0x7f070004;
         public static final int abc_search_url_text_normal=0x7f070000;
         public static final int abc_search_url_text_pressed=0x7f070002;
         public static final int abc_search_url_text_selected=0x7f070001;
+        public static final int yellow=0x7f070003;
     }
     public static final class dimen {
         /**  Default height of an action bar. 
@@ -1091,7 +1092,7 @@ containing a value of this type.
         public static final int action_menu_divider=0x7f050016;
         public static final int action_menu_presenter=0x7f050017;
         public static final int action_mode_close_button=0x7f050024;
-        public static final int action_settings=0x7f05003c;
+        public static final int action_settings=0x7f050043;
         public static final int activity_chooser_view_content=0x7f050025;
         public static final int always=0x7f05000b;
         public static final int beginning=0x7f050011;
@@ -1105,6 +1106,7 @@ containing a value of this type.
         public static final int end=0x7f050013;
         public static final int expand_activities_button=0x7f050026;
         public static final int expanded_menu=0x7f05002c;
+        public static final int forgotpassword=0x7f05003f;
         public static final int home=0x7f050014;
         public static final int homeAsUp=0x7f050005;
         public static final int icon=0x7f05002a;
@@ -1112,13 +1114,17 @@ containing a value of this type.
         public static final int image=0x7f050027;
         public static final int listMode=0x7f050001;
         public static final int list_item=0x7f050029;
+        public static final int login=0x7f050041;
         public static final int middle=0x7f050012;
         public static final int never=0x7f050009;
         public static final int none=0x7f050010;
         public static final int normal=0x7f050000;
+        public static final int password=0x7f05003d;
         public static final int progress_circular=0x7f050018;
         public static final int progress_horizontal=0x7f050019;
         public static final int radio=0x7f05002f;
+        public static final int register=0x7f050040;
+        public static final int saveCredentials=0x7f050042;
         public static final int search_badge=0x7f050032;
         public static final int search_bar=0x7f050031;
         public static final int search_button=0x7f050033;
@@ -1136,10 +1142,12 @@ containing a value of this type.
         public static final int split_action_bar=0x7f05001e;
         public static final int submit_area=0x7f050039;
         public static final int tabMode=0x7f050002;
+        public static final int textView1=0x7f05003c;
         public static final int title=0x7f05002b;
         public static final int top_action_bar=0x7f050020;
         public static final int up=0x7f050021;
         public static final int useLogo=0x7f050003;
+        public static final int username=0x7f05003e;
         public static final int withText=0x7f05000c;
     }
     public static final class integer {
@@ -1241,8 +1249,15 @@ containing a value of this type.
         public static final int abc_shareactionprovider_share_with_application=0x7f0a000b;
         public static final int action_settings=0x7f0a0010;
         public static final int app_name=0x7f0a000d;
+        public static final int forgotpassword=0x7f0a0014;
+        public static final int hello_taxisharer=0x7f0a0011;
         public static final int hello_world=0x7f0a000e;
+        public static final int login=0x7f0a0012;
+        public static final int password=0x7f0a0017;
+        public static final int register=0x7f0a0013;
+        public static final int rememberme=0x7f0a0015;
         public static final int title_activity_login=0x7f0a000f;
+        public static final int username=0x7f0a0016;
     }
     public static final class style {
         /** 
