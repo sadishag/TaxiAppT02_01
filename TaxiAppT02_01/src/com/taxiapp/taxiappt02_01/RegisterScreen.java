5a8c57e1e942d447963dba00b96207e0afde0c02
peggyle <peggyle.18@gmail.com>
2014-11-21 12:45:13 -0500

    added main menu and register screen xml

---
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/RegisterScreen.java
@@ -0,0 +1,15 @@
+package com.taxiapp.taxiappt02_01;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class RegisterScreen extends Activity {
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_register_screen);
+	}
+}
