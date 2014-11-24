374957d5f0efbb17bf7d7f241d448efbcae6b949
Sid Galappatti <sadishag@gmail.com>
2014-11-24 17:17:28 -0500

    CheckinScreen.java added with a layout for the check in screen. Links have been made in main, click login --> mainMenu, click checkin --> checkinScreen.

---
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/CheckinScreen.java
@@ -0,0 +1,35 @@
+package com.taxiapp.taxiappt02_01;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+
+public class CheckinScreen extends Activity {
+
+	private static String qrcode; // The string that should save qr code value
+
+	//add intent to call on zxing application: BarcodeScanner
+	public void checkinScreen(View view){
+		
+		Intent LaunchIntent = getPackageManager().getLaunchIntentForPackage("com.google.zxing.client.android");
+		startActivity(LaunchIntent);
+	}
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_checkin_screen);
+	}
+
+	//getter for qrcode
+	public String getQRCode(){
+		return qrcode;
+	}
+
+}
--- TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/MainMenu.java
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/MainMenu.java
@@ -1,9 +1,17 @@
 package com.taxiapp.taxiappt02_01;
 
 import android.app.Activity;
+import android.app.Fragment;
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.view.ViewGroup;
+import android.widget.Button;
 
 public class MainMenu extends Activity {
 
@@ -11,5 +19,35 @@ public class MainMenu extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main_menu);
+		addListenerOnButton();
+		
 	}
+	
+	private void addListenerOnButton(){
+		
+		final Context context = this;
+		
+		Button button = (Button) findViewById(R.id.checkin);
+		
+		button.setOnClickListener(new OnClickListener() {
+
+			@Override
+			public void onClick(View v) {
+				Intent intent = new Intent(context, CheckinScreen.class);
+				startActivity(intent);
+				
+			}
+			
+		});
+		
+	}
+	
+//	private void checkinScreenLaunch(View view){
+//		
+//		// link Checkin button to CheckinScreen.
+//		Intent checkinIntent = new Intent(this, CheckinScreen.class);
+//		startActivity(checkinIntent);
+//	}
+	
+	
 }
