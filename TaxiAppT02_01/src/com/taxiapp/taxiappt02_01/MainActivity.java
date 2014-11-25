15c78aab8e8ca0da24f5588d4fb7d574f88a2c68
kenwang35 <khenwang@hotmail.com>
2014-11-25 12:33:07 -0500

    updated icon and added files

--- TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/MainActivity.java
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/MainActivity.java
@@ -10,6 +10,6 @@ public class MainActivity extends Activity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_register_screen);
+        setContentView(R.layout.activity_book_ride);
     }
 }
