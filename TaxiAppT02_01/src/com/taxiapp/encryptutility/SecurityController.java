285fb2f9b1c4dd45291f62628f3efb850c5fc9ae
Sid Galappatti <sadishag@gmail.com>
2014-12-02 16:49:07 -0500

    Had to reimport zxing library and added securityController

---
+++ TaxiAppT02_01/src/com/taxiapp/encryptutility/SecurityController.java
@@ -0,0 +1,20 @@
+package com.taxiapp.encryptutility;
+
+import org.apache.commons.codec.binary.Base64;
+
+public class SecurityController {
+	
+	public String encodeString(String string) {
+		byte[] encodedByteArray = Base64.encodeBase64(string.getBytes());
+		String encodedString = new String(encodedByteArray);
+		return encodedString;
+	}
+	
+	public String decodeString(String encryptedString) {
+		byte[] decodedByteArray = Base64.decodeBase64(encryptedString.getBytes());
+		String decodedString = new String(decodedByteArray);
+		return decodedString;
+	}
+	
+
+}
