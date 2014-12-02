675eac43ab487e9bf9476c058cd5c11aeec572a9
Sid Galappatti <sadishag@gmail.com>
2014-12-02 12:44:02 -0500

    Deleted a dependency issue from appcompat a jar file

--- TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/RideInProgressScreen.java
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/RideInProgressScreen.java
@@ -14,6 +14,11 @@ import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
+//import com.google.android.gms.maps.CameraUpdateFactory;
+//import com.google.android.gms.maps.GoogleMap;
+//import com.google.android.gms.maps.MapFragment;
+//import com.google.android.gms.maps.model.LatLng;
+//import com.google.android.gms.maps.model.MarkerOptions;
 
 public class RideInProgressScreen extends Activity {
 
@@ -39,6 +44,22 @@ public class RideInProgressScreen extends Activity {
             map.addMarker(new MarkerOptions()
                     .title("You Are Here")
                     .position(userLocation));
+//            GoogleMap map = ((MapFragment) getFragmentManager()
+//                    .findFragmentById(R.id.map)).getMap();
+//            
+//            LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+//            Criteria criteria = new Criteria();
+//            String provider = service.getBestProvider(criteria, false);
+//            Location location = service.getLastKnownLocation(provider);
+//            LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+//            
+//
+//            map.setMyLocationEnabled(true);
+//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13));
+//
+//            map.addMarker(new MarkerOptions()
+//                    .title("You Are Here")
+//                    .position(userLocation));
         
     }
 }
