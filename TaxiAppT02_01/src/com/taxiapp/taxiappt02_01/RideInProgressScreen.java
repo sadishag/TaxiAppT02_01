580c7087a1fe0f121a8061baaf2aa2c9313fd2e3
Sid Galappatti <sadishag@gmail.com>
2014-12-03 04:18:10 -0500

    Got location working.. hopefully

--- TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/RideInProgressScreen.java
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/RideInProgressScreen.java
@@ -1,11 +1,22 @@
 package com.taxiapp.taxiappt02_01;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.GooglePlayServicesClient;
+import com.google.android.gms.location.LocationClient;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
 import android.app.Activity;
 import android.location.Criteria;
 import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 
@@ -15,30 +26,69 @@ import android.view.MenuItem;
 //import com.google.android.gms.maps.model.LatLng;
 //import com.google.android.gms.maps.model.MarkerOptions;
 
-public class RideInProgressScreen extends Activity {
+public class RideInProgressScreen extends FragmentActivity implements
+GooglePlayServicesClient.ConnectionCallbacks,
+GooglePlayServicesClient.OnConnectionFailedListener {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_ride_in_progress_screen);
+	private static final String TAG = "RideInProgressScreen";
 
-            // Get a handle to the Map Fragment
-//            GoogleMap map = ((MapFragment) getFragmentManager()
-//                    .findFragmentById(R.id.map)).getMap();
-//            
-//            LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
-//            Criteria criteria = new Criteria();
-//            String provider = service.getBestProvider(criteria, false);
-//            Location location = service.getLastKnownLocation(provider);
-//            LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
-//            
-//
-//            map.setMyLocationEnabled(true);
-//            map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13));
-//
-//            map.addMarker(new MarkerOptions()
-//                    .title("You Are Here")
-//                    .position(userLocation));
-        
-    }
+	// Global variable to hold the current location
+	//	Location mCurrentLocation;
+	//	
+	//	mCurrentLocation = mLocationClient.getLastLocation();
+	//	
+	//
+	//	public Location getCurrentLocation(){
+	//		
+	//		return mCurrentLocation;
+	//	}
+	//	
+
+
+
+
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.activity_ride_in_progress_screen);
+
+
+
+
+		// Get a handle to the Map Fragment
+		GoogleMap map = ((MapFragment) getFragmentManager().findFragmentById(R.id.map)).getMap();
+
+		LocationManager service = (LocationManager) getSystemService(LOCATION_SERVICE);
+		Criteria criteria = new Criteria();
+		String provider = service.getBestProvider(criteria, false);
+		Location location = service.getLastKnownLocation(provider);
+		LatLng userLocation = new LatLng(location.getLatitude(),location.getLongitude());
+
+
+		map.setMyLocationEnabled(true);
+		map.moveCamera(CameraUpdateFactory.newLatLngZoom(userLocation, 13));
+
+		map.addMarker(new MarkerOptions()
+		.title("You Are Here")
+		.position(userLocation));
+
+	}
+
+	@Override
+	public void onConnectionFailed(ConnectionResult arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onConnected(Bundle arg0) {
+		// TODO Auto-generated method stub
+
+	}
+
+	@Override
+	public void onDisconnected() {
+		// TODO Auto-generated method stub
+
+	}
 }
