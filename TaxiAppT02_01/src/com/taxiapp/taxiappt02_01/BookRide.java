4c520bfa4dc8a92580c7701a55ad988b4c37b1e6
kenwang35 <khenwang@hotmail.com>
2014-12-04 00:14:11 -0500

    update

--- TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/BookRide.java
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/BookRide.java
@@ -1,17 +1,43 @@
 package com.taxiapp.taxiappt02_01;
 
-import android.app.AliasActivity;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class BookRide extends AliasActivity {
-
+public class BookRide extends Activity {
+	
+	static TextView date;
+	Button bookButton;
+	EditText toBook, fromBook;
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_book_ride);
 		
+		bookButton = (Button) findViewById(R.id.submitButton);
+		date = (TextView) findViewById(R.id.datebook);
+		toBook = (EditText) findViewById(R.id.tobook);
+		fromBook = (EditText) findViewById(R.id.frombook);
+		
+		SimpleDateFormat sdf = new SimpleDateFormat("yyyy/MM/dd");
+		String currentDateandTime = sdf.format(new Date());
+		
+		date.setText(currentDateandTime);
+		date.setTextColor(Color.GRAY);
+		
+	}
+	
+	public void dateOnClick(View view){
+		Intent calendarIntent = new Intent(BookRide.this, CalendarScreen.class);
+		startActivity(calendarIntent);
 	}
 
 }
--- TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/OfferTaxi.java
+++ TaxiAppT02_01/src/com/taxiapp/taxiappt02_01/OfferTaxi.java
@@ -33,7 +33,7 @@ public class OfferTaxi extends Activity {
 			
 			@Override
 			public void onClick(View v) {
-				Intent intent = new Intent(OfferTaxi.this, CalendarScreen.class);
+				Intent intent = new Intent(OfferTaxi.this, BookRide.class);
                 startActivity(intent);  				
 			}
 		});
